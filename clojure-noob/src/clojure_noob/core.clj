e9fe3a9bf6e3822b0089f30f661a3f86ab933e70
webdude21 <dimo.petrof@gmail.com>
2016-09-14 00:50:43 +0300

    not sure what i did

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,6 +4,11 @@
 (defn -main
   [& args]
 
+  ;; curryable pow function
+  (defn pow
+    [x]
+      (fn [y] (int (Math/pow y x)))
+    )
 
   ;; curryable sum function
   (defn sum
@@ -17,9 +22,12 @@
 
   ;; curried version of sum with 2
   (def sumWithTwo (sum 2))
+  ;; curried version to expand a value to the power of 10
+  (def toThePowerOf10 (pow 10))
 
   (def someNumbers [60 80 100 400])
 
   (printf "Average of %s is %d%n" someNumbers (avarage someNumbers))
   (println (map sumWithTwo [1 3 4 5 6]))
+  (println (map toThePowerOf10 [1 3 4 5 6]))
 )
