e488a3d4e6d7959f44b2c4eb5317d91626dc06e7
webdude21 <dimo.petrof@gmail.com>
2016-09-14 00:09:44 +0300

    some more (its kinda cool)

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,9 +4,22 @@
 (defn -main
   [& args]
 
+
+  ;; curryable sum function
+  (defn sum
+    [x]
+      (fn [y] (+ x y))
+    )
+
   (defn avarage
     [numbers]
       (/ (apply + numbers) (count numbers)))
 
-  (printf "Result: %d%n" (avarage [60 80 100 400]))
+  ;; curried version of sum with 2
+  (def sumWithTwo (sum 2))
+
+  (def someNumbers [60 80 100 400])
+
+  (printf "Average of %s is %d%n" someNumbers (avarage someNumbers))
+  (println (map sumWithTwo [1 3 4 5 6]))
 )
