14ee549eba291d36d40ee70738dbca6856dc898a
webdude21 <dimo.petrof@gmail.com>
2016-09-14 07:59:31 +0300

    changed the test a bit, still doesn't work

--- clojure-noob/test/clojure_noob/core_test.clj
+++ clojure-noob/test/clojure_noob/core_test.clj
@@ -1,7 +1,9 @@
-(ns clojure-noob.core-test
+(ns clojure_noob.core_test
   (:require [clojure.test :refer :all]
-            [clojure-noob.core :refer :all]))
+            [clojure_noob.core :refer :all])
+  )
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Average function works just fine"
+    (is (= (avarage [1 2 3 4 -1 3]) 12)))
+  )
