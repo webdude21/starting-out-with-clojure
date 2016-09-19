cfc9861a85b73ad5a2c3bba9fe40cd8a8df82c99
webdude21 <dimo.petrof@gmail.com>
2016-09-19 22:05:25 +0300

    refactored tests completely (reduced code duplication)

--- clojure-noob/test/clojure_noob/core_test.clj
+++ clojure-noob/test/clojure_noob/core_test.clj
@@ -9,29 +9,32 @@
 
 (deftest a-test-common-problems-factorial
   (testing "Factorial function works just fine"
-    (is (= (factorial 1) 1))
-    (is (= (factorial 5) 120))
-    (is (= (factorial 15) 1307674368000))))
+    (are [input expected]
+      (= (factorial input) expected)
+      1 1
+      5 120
+      15 1307674368000)))
 
 (deftest a-test-common-problems-fibonacci
   (testing "Fibonacci function works just fine"
-    (are [actual expected]
-      (= actual expected)
-      (fibonacci 2) 1
-      (fibonacci 2) 1
-      (fibonacci 0) 0
-      (fibonacci 1) 1
-      (fibonacci 7) 13
-      (fibonacci 10) 55
-      (fibonacci 18) 2584
-      (fibonacci 26) 121393
-      (fibonacci 150) 9969216677189303386214405760200N)))
+    (are [input expected]
+      (= (fibonacci input) expected)
+      0 0
+      1 1
+      2 1
+      7 13
+      10 55
+      18 2584
+      26 121393
+      150 9969216677189303386214405760200N)))
 
 (deftest a-test-common-problems-prime-numbers
   (testing "prime? function works just fine"
-    (is (= (prime? 2) true))
-    (is (= (prime? 5) true))
-    (is (= (prime? 8) false))
-    (is (= (prime? 4) false))
-    (is (= (prime? 73) true))
-    (is (= (prime? 75) false))))
+    (are [input expected]
+      (= (prime? input) expected)
+      2 true
+      5 true
+      8 false
+      4 false
+      73 true
+      75 false)))
