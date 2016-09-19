ba51c5ec3f626ebc16401be158d6244296bec498
webdude21 <dimo.petrof@gmail.com>
2016-09-19 20:55:13 +0300

    setup cursive for project

--- clojure-noob/src/clojure_noob/common_problems.clj
+++ clojure-noob/src/clojure_noob/common_problems.clj
@@ -4,13 +4,14 @@
   (reduce * (range 1 (inc n))))
 
 (def fibonacci
-  (memoize (fn [n]
-    (if (< n 2) n
-      (+' (fibonacci (dec n)) (fibonacci(- n 2)))))))
+  (memoize
+    (fn [n]
+      (if (< n 2)
+        n
+        (+' (fibonacci (dec n)) (fibonacci (- n 2)))))))
 
 (defn divisableOf? [n]
-    (fn [divisor] (= (rem n divisor) 0)))
+  (fn [divisor] (= (rem n divisor) 0)))
 
 (defn prime? [n]
-  (def divisorOfN? (divisableOf? n))
-    (not-any? divisorOfN? (range 2 (inc (int (Math/sqrt n))))))
+  (not-any? (divisableOf? n) (range 2 (inc (int (Math/sqrt n))))))
