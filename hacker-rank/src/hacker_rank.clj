395660e2e408216a85274633d9f217ed65498383
webdude21 <dimo.petrof@gmail.com>
2016-09-21 18:23:36 +0300

    filter odd in vector

--- hacker-rank/src/hacker_rank.clj
+++ hacker-rank/src/hacker_rank.clj
@@ -3,5 +3,8 @@
   (:require [problems :refer :all]))
 
 (defn -main [& args]
-    (println (problems/listreplication 5 [1, 2, 3]))
-    (println (problems/filterArray 5 [1, 2, 3, 0, 14, 5, 4])))
+    (def test-array [1, 2, 3, 0, 14, 5, 4])
+
+    (println (problems/list-replication 5 test-array))
+    (println (problems/filter-array 5 test-array))
+    (println (problems/filter-odd test-array)))
--- hacker-rank/src/problems.clj
+++ hacker-rank/src/problems.clj
@@ -1,12 +1,17 @@
 (ns problems)
 
 ;List Replication
-(def listreplication
+(def list-replication
   (fn [num lst]
-    (apply concat
-      (map (fn [x] (repeat num x)) lst))))
+    (apply concat (map (fn [x] (repeat num x)) lst))))
 
 ;Filter Array
-(def filterArray
+(def filter-array
   (fn [delim lst]
     (filter #(< % delim) lst)))
+
+;Filter out odd
+
+(def filter-odd
+  (fn [lst]
+    (keep-indexed #(if (odd? %1) %2) lst)))
