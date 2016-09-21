29100beef828efef5fbc47aab3ee4b6e3492a5a0
webdude21 <dimo.petrof@gmail.com>
2016-09-21 18:43:12 +0300

    add few more 'challenges'

--- hacker-rank/src/hacker_rank.clj
+++ hacker-rank/src/hacker_rank.clj
@@ -3,8 +3,9 @@
   (:require [problems :refer :all]))
 
 (defn -main [& args]
-    (def test-array [1, 2, 3, 0, 14, 5, 4])
-
-    (println (problems/list-replication 5 test-array))
-    (println (problems/filter-array 5 test-array))
-    (println (problems/filter-odd test-array)))
+    (def test-vector [1, 2, 3, 0, 14, 5, 4])
+    (println (problems/list-replication 5 test-vector))
+    (println (problems/filter-list 5 test-vector))
+    (println (problems/filter-odd test-vector))
+    (println (problems/item-replication 5))
+    (println (problems/sum-odd test-vector)))
--- hacker-rank/src/problems.clj
+++ hacker-rank/src/problems.clj
@@ -5,13 +5,27 @@
   (fn [num lst]
     (apply concat (map (fn [x] (repeat num x)) lst))))
 
-;Filter Array
-(def filter-array
+;Filter list
+(def filter-list
   (fn [delim lst]
     (filter #(< % delim) lst)))
 
 ;Filter out odd
-
 (def filter-odd
   (fn [lst]
     (keep-indexed #(if (odd? %1) %2) lst)))
+
+;List of n elements
+(def item-replication
+  (fn [n]
+    (repeat n n)))
+
+;List reverse
+(def list-reverse
+  (fn [lst]
+    (reverse lst)))
+
+;Sum of odd elements
+(def sum-odd
+  (fn [lst]
+    (reduce + (filter odd? lst))))
